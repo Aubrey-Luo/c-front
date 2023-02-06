41acf18eee97c24deb8ebb185ff87beb2cbf8df9
luohao <luo.hao_t03178@hellogroup.com>
2023-02-06 21:01:31 +0800

    feat: README

---
+++ src/libs/svg-icon/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg aria-hidden="true">
+    <use :xlink:href="name" />
+  </svg>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  // 显示的 svg
+  name: {
+    type: String,
+    required: true
+  },
+  // svg 图标的颜色
+  color: {
+    type: String
+  },
+  // tailwind 指定 svg 颜色的类名
+  fillClass: {
+    type: String
+  }
+})
+
+// 真实显示的 svg 图标（拼接 #icon-）
+computed()
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   // tailwind 应用范围
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
-    extend: {}
+    extend: {
+      fontSize: {
+        xs: ['0.25rem', '0,35rem'],
+        sm: ['0.25rem', '0,35rem'],
+        base: ['0.25rem', '0,35rem'],
+        lg: ['0.25rem', '0,35rem'],
+        xl: ['0.25rem', '0,35rem']
+      }
+    }
   },
   plugins: []
 }
