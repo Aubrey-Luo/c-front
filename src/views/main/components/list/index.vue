9d38fad481f64434d6375b8c1819d4e8c618d176
Aubrey-Luo <luohao1er@gmail.com>
2023-04-22 18:11:54 +0800

    feat: 图片展示基础ui开发

---
+++ src/views/main/components/list/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <item-vue v-for="item in pexelsList" :key="item.id" :data="item"></item-vue>
+  </div>
+</template>
+
+<script setup>
+import { getPexelsList } from '@/api/pexels'
+import { ref } from 'vue'
+import itemVue from './item.vue'
+
+/**
+ * 构建数据请求
+ */
+let query = {
+  page: 1,
+  size: 20
+}
+
+const pexelsList = ref([])
+const getPexelsData = async () => {
+  const res = await getPexelsList(query)
+  pexelsList.value = res.list
+}
+getPexelsData()
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div class="h-full overflow-auto bg-white dark:bg-zinc-800 duration-500">
     <navigation-vue />
+    <div class="max-w-screen-xl mx-auto relative m-1 xl:mt-4">
+      <list-vue></list-vue>
+    </div>
   </div>
 </template>
 
 <script setup>
 import navigationVue from './components/navigation/index.vue'
+import ListVue from './components/list/index.vue'
 import { useStore } from 'vuex'
 const store = useStore()
 // 触发 category 数据获取动作
